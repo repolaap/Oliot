8e55dd2a4275674a14ca3849dadc3df5a5527e8b
repolaap <80278189+repolaap@users.noreply.github.com>
2022-03-10 18:05:55 +0200

    Add files via upload

---
+++ WK7/T4/src/main/java/com/example/t4/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.t4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = findViewById(R.id.textView);
+        EditText input = findViewById(R.id.textInput);
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                text.setText(editable.toString());
+            }
+        });
+    }
+
+
+
+}
